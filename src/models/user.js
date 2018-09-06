75c0451313f69acd1de66b36a9f557f980817eba
Raúl Montes <raul@mtblc.co>
2018-09-05 22:22:24 -0300

    Initial user model
    Based on this command: ./node_modules/.bin/sequelize model:generate --name user --attributes firstName:string,lastName:string,email:string,password:string

---
+++ src/models/user.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {});
+  return user;
+};
