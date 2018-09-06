81d33f9bf07309f21f153a858e813675d2e161a2
Raúl Montes <raul@mtblc.co>
2018-09-05 22:34:27 -0300

    Add create, retrieve, update and delete routes for user model

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,4 +6,31 @@ router.get('users', '/', async (ctx) => {
   ctx.body = await ctx.orm.user.findAll();
 });
 
+router.post('users-create', '/', async (ctx) => {
+  await ctx.orm.user.create(ctx.request.body);
+  ctx.redirect(ctx.router.url('users'));
+});
+
+router.get('users-show', '/:id', async (ctx) => {
+  const user = await ctx.orm.user.findById(ctx.params.id);
+  ctx.assert(user, 404);
+  ctx.body = user;
+});
+
+router.patch('users-update', '/:id', async (ctx) => {
+  const user = await ctx.orm.user.findById(ctx.params.id);
+  ctx.assert(user, 404);
+  ctx.body = await user.update(
+    ctx.request.body,
+    { fields: ['firstName', 'lastName', 'password'] },
+  );
+});
+
+router.delete('users-destroy', '/:id', async (ctx) => {
+  const user = await ctx.orm.user.findById(ctx.params.id);
+  ctx.assert(user, 404);
+  await user.destroy();
+  ctx.redirect(ctx.router.url('users'));
+});
+
 module.exports = router;
