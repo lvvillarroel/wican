97fe8723b52829ca3c7d404986c0107f646aeccc
Raúl Montes <raul@mtblc.co>
2018-09-08 19:13:25 -0300

    Avoid repeating user fetch by :id param

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,13 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.param('id', async (id, ctx, next) => {
+  const user = await ctx.orm.user.findById(ctx.params.id);
+  ctx.assert(user, 404);
+  ctx.state.user = user;
+  return next();
+});
+
 router.get('users', '/', async (ctx) => {
   ctx.body = await ctx.orm.user.findAll();
 });
@@ -12,24 +19,18 @@ router.post('users-create', '/', async (ctx) => {
 });
 
 router.get('users-show', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  ctx.body = user;
+  ctx.body = ctx.state.user;
 });
 
 router.patch('users-update', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  ctx.body = await user.update(
+  ctx.body = await ctx.state.user.update(
     ctx.request.body,
     { fields: ['firstName', 'lastName', 'password'] },
   );
 });
 
 router.delete('users-destroy', '/:id', async (ctx) => {
-  const user = await ctx.orm.user.findById(ctx.params.id);
-  ctx.assert(user, 404);
-  await user.destroy();
+  await ctx.state.user.destroy();
   ctx.redirect(ctx.router.url('users'));
 });
 
